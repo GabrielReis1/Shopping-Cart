6d106cdea5dbe6029d3ff289bdab186af532e055
GabrielReis1 <gabrielreis.santos@outlook.com>
2023-01-16 15:25:57 -0300

    requisito 1

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -1,21 +1,27 @@
 import './mocks/fetchSimulator';
 import { fetchProductsList } from '../helpers/fetchFunctions';
 import computadorSearch from './mocks/search';
+import { it } from 'mocha';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList()).toBe('function');
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
-
+    expect(fetchProductsList).toBeCalled('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   });
 
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+    expect(fetchProductsList).toBeCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=$QUERY')
   });
-  //iniciando!!
-  // it('...', () => {
-  // });
+
+  it('Testa se o retorno da função fetchProductsList com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', () => {
+    expect(fetchProductsList('computador')).toEqual(computadorSearch);
+  });
+
+  it('Testa se, ao chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: "Termo de busca não informado".', () => {
+    expect(fetchProductsList('')).toThrow('Termo de busca não informado')
+  });
 });
