d1cd75a36655b2e27740a14bed4d5f8040bfd3b1
GabrielReis1 <gabrielreis.santos@outlook.com>
2023-01-18 16:36:15 -0300

    requisito 5

--- src/main.js
+++ src/main.js
@@ -8,16 +8,23 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 const products = document.querySelector('.products');
 
 const createProducts = async () => {
-  const data = await fetchProductsList('computador');
-  products.innerHTML = '';
-  data.forEach((id) => products.appendChild(createProductElement(id)));
+  try {
+    const data = await fetchProductsList('computador');
+    products.innerHTML = '';
+    data.forEach((id) => products.appendChild(createProductElement(id)));
+  } catch (error) {
+    const errorAPI = document.createElement('span');
+    errorAPI.className = 'error';
+    errorAPI.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente.';
+    products.appendChild(errorAPI);
+  }
 };
 createProducts();
 
 const createLoading = () => {
   const loading = document.createElement('span');
   loading.className = 'loading';
-  loading.innerHTML = 'carregando...';
+  loading.innerText = 'carregando...';
   products.appendChild(loading);
 };
 createLoading();
